3c5dc8daa3797220297f224e79c87ccb052756b4
noobidoo <ttraskback@gmail.com>
2011-12-25 02:23:32 +0200

    Added stub for polygonarea class

--- Polygonia/src/net/crafthub/ddl2829/polygonia/PolygonArea.java
+++ Polygonia/src/net/crafthub/ddl2829/polygonia/PolygonArea.java
@@ -25,6 +25,9 @@ package net.crafthub.ddl2829.polygonia;
 
 import java.awt.Polygon;
 import java.awt.geom.Rectangle2D;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,9 +40,9 @@ import org.bukkit.Location;
 
 public class PolygonArea {
     private Polygon area;
-    private Map<CFlags,Boolean> flags;
-    private Map<String,Map<CFlags,Boolean>> PlayerFlags;
-    private Set<String> Owners;
+    private Map<CFlags,Boolean> flags = new EnumMap<CFlags,Boolean>(CFlags.class);
+    private Map<String,Map<CFlags,Boolean>> PlayerFlags = new HashMap<String,Map<CFlags,Boolean>>();
+    private Set<String> Owners = new HashSet<String>();
     private Integer MaxY;
     
     public PolygonArea(Polygon area, Set<String> Owners, Integer MaxY, Integer MinY) {
@@ -52,12 +55,24 @@ public class PolygonArea {
         return area.getBounds2D();
     }
     
+    /**
+     * Checks if a Location is in the current area.
+     * 
+     * @param cord The Location of the block
+     * @return If area contains Location returns true
+     */
     public boolean isBlockInPolygon(Location cord){
-        throw new UnsupportedOperationException("Not supported yet.");
+        return area.contains(cord.getBlockX(),cord.getBlockZ());
     }
     
     public void setPlayerFlag(String name, CFlags flag,Boolean value){
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (!PlayerFlags.containsKey(name)) {
+            Map<CFlags,Boolean> temp = new EnumMap<CFlags, Boolean>(CFlags.class);
+            temp.put(flag, value);
+            PlayerFlags.put(name, temp);
+        }else{
+            PlayerFlags.get(name).put(flag, value);
+        }
     }
     
     public void setFlag(CFlags flag,Boolean value){
@@ -68,10 +83,21 @@ public class PolygonArea {
         return Owners;
     }
 
+    /**
+     * Get the full map of players and flags
+     * 
+     * @return All players and there flags in a <code>Map<String, Map<CFlags, Boolean>></code>
+     */
     public Map<String, Map<CFlags, Boolean>> getPlayerFlags() {
         return PlayerFlags;
     }
 
+    /**
+     * Get a specific players flags
+     * 
+     * @param player player name
+     * @return A Map of CFlags and boolean
+     */
     public Map<CFlags, Boolean> getPlayerFlags(String player) {
         return PlayerFlags.get(player);
     }
