9e9a3387a7796f130cbd6bf8c24cd7652923e066
noobidoo <ttraskback@gmail.com>
2011-12-28 02:53:17 +0200

    More testing

--- Polygonia/src/net/crafthub/ddl2829/polygonia/main.java
+++ Polygonia/src/net/crafthub/ddl2829/polygonia/main.java
@@ -24,9 +24,17 @@
 package net.crafthub.ddl2829.polygonia;
 
 import java.awt.Polygon;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.Location;
 
 /**
@@ -40,19 +48,34 @@ public class main {
         Random randomGenerator = new Random();
         Set<String> Owners= new HashSet<String>();
         Owners.add("Noobidoo");
-        for(int i=0;i==50000;i++){
-            Integer xorgs=randomGenerator.nextInt(1000000);
-            Integer yorgs=randomGenerator.nextInt(1000000);
-            int[] x = {getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator)};  
-            int[] y = {getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator)};  
-            handler.addArea(new PolygonArea(new Polygon(x, y, x.length),Owners, 70, 64));
-        }
-        for (int testpoint=0;testpoint==100000;testpoint++){
-            Integer xorgs=randomGenerator.nextInt(1000000);
-            Integer yorgs=randomGenerator.nextInt(1000000);
-            Location loc = new Location(null, xorgs, 64, yorgs);
-            Set<PolygonArea> areasAfected = handler.getAreasAfected(loc);
-        }
+        Integer xorgs=randomGenerator.nextInt(1000000);
+        Integer yorgs=randomGenerator.nextInt(1000000);
+        int[] x = {getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator),getRandomInteger(xorgs-1000,xorgs+1000,randomGenerator)};  
+        int[] y = {getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator),getRandomInteger(yorgs-1000,yorgs+1000,randomGenerator)};  
+        
+        PolygonArea area = new PolygonArea(new Polygon(x, y, x.length),Owners, 70, 64,"Test area");
+        //handler.addArea(area);
+        try {
+            //area = handler.getAreaByName("Test area");
+            System.out.println(Arrays.toString(area.getArea().xpoints));
+            System.out.println(Arrays.toString(area.getArea().ypoints));
+            FileOutputStream fos = null;
+            FileInputStream fis = null;
+            ObjectOutputStream out = null;
+            ObjectInputStream in = null;
+            fos = new FileOutputStream(area.getName()+".area");
+            out = new ObjectOutputStream(fos);
+            out.writeObject(area);
+            out.close();
+            fis = new FileInputStream(area.getName()+".area");
+            in = new ObjectInputStream(fis);
+            PolygonArea area2 = (PolygonArea)in.readObject();
+            in.close();
+            System.out.println(Arrays.toString(area2.getArea().xpoints));
+            System.out.println(Arrays.toString(area2.getArea().ypoints));
+        } catch (Exception ex) {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+        }   
     }
     private static Integer getRandomInteger(int aStart, int aEnd, Random aRandom){
     if ( aStart > aEnd ) {
